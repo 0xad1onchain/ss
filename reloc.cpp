f7664a172a97c8baa46ad18043aa584c663edeed
Aditya Gupta <adi.gupta13@gmail.com>
2018-04-13 13:39:13 +0530

    Update reloc.cpp

--- reloc.cpp
+++ reloc.cpp
@@ -16,23 +16,23 @@ void parse_src()
 
     unsigned int newstart = 0,oldstart = 0,addr,opcode, inc,bmask;
 
-    //get new start address
+    
     cout << "Enter the actual starting address" << endl;
     cin >>hex>>newstart;
 
-    //i/o files
+    
     input_file.open("reloc.txt", ios::in );
     output_file.open("reloc_output.txt", ios::out);
 
-    while(getline(input_file,line)) //read each line
+    while(getline(input_file,line)) 
     {
-       stringstream tokens; //stringstream for tokenisation
+       stringstream tokens; 
        addr = 0;
-       tokens<<line; //add line to string stream
+       tokens<<line;
        if(line[0] == 'H'){
-            tokens>>temp>>temp; //read H and PROG name to temp
-            tokens>>hex>>oldstart;  //Read original start address
-            inc = newstart-oldstart; //compute increment
+            tokens>>temp>>temp;
+            tokens>>hex>>oldstart;
+            inc = newstart-oldstart;
             continue;
         }
         if(line[0]== 'E')
@@ -41,27 +41,27 @@ void parse_src()
         }
         else if(line[0] == 'T')
         {
-            tokens>>temp;//skip T char
-            //read start address and bit mask
+            tokens>>temp;
+            
             tokens>>hex>>addr;
             tokens>>hex>>bmask;
-            //update start address
+            
             addr+=inc;
             while(tokens>>hex>>opcode)
             {
-                //for each opcode check if modification is needed
+                
                 if(bmask & 0x800)
                 {
-                    //if modification is needed, add increment
+                    
                     opcode+=inc;
                 }
                 output_file<<uppercase<<hex<<addr<<"\t"<<hex<<opcode<<endl;
-                //update location counter by calculating length of opcode digits
+                
                 stringstream hexdigit;
                 hexdigit<<hex<<opcode;
                 addr+=hexdigit.str().length()/2;
 
-                bmask= bmask<<1;//shift bitmask by 1 for next opcode
+                bmask= bmask<<1;
             }
         }
     }
